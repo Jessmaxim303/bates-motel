c4640d9337c9210966e72cd4d5abd48f55429f10
Jesse Maxim <jessmaxim303@gmail.com>
2019-10-31 19:12:46 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import Person from "./Person";
+// import Customer from "./Customer";
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// 1) Total Rooms Available for today’s date
+// 2) Total revenue for today’s date
+// 3) Percentage of rooms occupied for today’s date
+
+function getData(type) {
+	const root = 'https://fe-apps.herokuapp.com/api/v1/overlook/1904';
+	const url = `${root}${type}`;
+	console.log(url)
+	const promise = fetch(url)
+	                .then(data => data.json());
+	                // console.log(promise)
+	return promise;
+}
+
+getData('/users/users').then(function(user) {
+	const users = user.users;
+	users.forEach(function(user) {
+		// console.log(user.name)
+	})
+
+	// console.log(users[0].name)
+
+	$('.js_login-submit').on('click', function() {
+    if ($('.user_name').val() === 'manager' && $('.user_pswd').val() === 'overlook2019') {
+      window.location = "./manager.html";
+  } else if ($('.user_name').val() === 'customer50' && $('.user_pswd').val() === 'overlook2019') {
+      $('#js_insert-name').text(users[0].name)
+  }
+});
+
+
+});  //End of getData
+
+
+//   $('.js_login-submit').on('click', function() {
+//     if ($('.user_name').val() === 'manager' && $('.user_pswd').val() === 'overlook2019') {
+//       window.location = "./manager.html";
+//   } else if ($('.user_name').val() === 'customer50' && $('.user_pswd').val() === 'overlook2019') {
+//       $('#js_insert-name').text('user.name')
+//   }
+// });
+
+ 
